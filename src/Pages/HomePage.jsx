bc9324314e132aad188cb0950b2479fb2513f07e
Janak Bahadur Bohara <janakcustomx@gmail.com>
2024-07-28 14:49:24 +0545

    add: connect FE and BE using proxy, featch, loaddata, frontend and backend server

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,17 +1,30 @@
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product.jsx";
-import products from "../data.js";
+import { useEffect, useState } from "react";
+
+
 
 
 const HomePage = () => {
-    return(
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+      // Connect to frontEnd and BackEnd data
+      // /api means vite.config.js server proxy /api: "http://localhost:5000"
+      fetch("/api/v1/products")
+      .then((resp) => resp.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.log("Error::", err.message));
+  }, [])
+    
+   return(
         <>
         <h1>Latest Products after vite localhost success  "dev": "vite --host"</h1>
         <Row>
           {
             products.map(product => (
               <Col sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
+                <Product product={product} key={product.id}/>
               </Col>
             ))
           }
