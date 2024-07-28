59f99cf2a5d9204662e76ba2416ae75f94b2b4dc
Janak Bahadur Bohara <janakcustomx@gmail.com>
2024-07-28 11:37:45 +0545

    add: createRoutesFromElements, Route, path, element

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { RouterProvider, createBrowserRouter} from "react-router-dom";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import HomePage from "./Pages/HomePage.jsx";
 import ProductPage from "./Pages/ProductPage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <App/>,
-    children: [
-      {
-        path: "",
-        element: <HomePage/>
-      },
-      {
-        path: "/product",
-        element: <ProductPage/>
-      }
-    ]
-  }
-])
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App/>}>
+    <Route path="" element={<HomePage/>}></Route>
+    <Route path="product" element={<ProductPage/>}></Route>
+  </Route>
+))
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 <RouterProvider router={router}></RouterProvider>
